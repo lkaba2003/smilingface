cf18e3fd169e258fcb3485d22f3ccf279607f73b
Lamine Kaba <Lkaba2003@yahoo.com>
2014-11-25 13:27:09 -0600

    Precompiled the assset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,8 +84,8 @@ Rails.application.configure do
   :address                => 'smtp.sendgrid.net',
   :port                   => '587',
   :authentication         => :plain,
-  :user_name              => ENV[''],
-  :password               => ENV[''],
+  :user_name              => ENV['app31721069@heroku.com'],
+  :password               => ENV['mnxk5pyj'],
   :domain                 => 'heroku.com',
   :enable_starttls_auto   => true
 } 
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  # config.secret_key = 'efa026308690aa6a55b6ee604ea199591f62235badd715799d0e55a194f96442606bd2c0bd6408dc232f6749daa73c1ce1a13482e08d6321e78310f0efda66d6'
+   config.secret_key = 'efa026308690aa6a55b6ee604ea199591f62235badd715799d0e55a194f96442606bd2c0bd6408dc232f6749daa73c1ce1a13482e08d6321e78310f0efda66d6'
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
