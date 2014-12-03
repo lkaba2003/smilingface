d0b8938e4265bd8ed08de3a24be4d178e8621e4e
Lamine Kaba <Lkaba2003@yahoo.com>
2014-12-03 17:09:25 -0600

    All

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider               => 'AWS',
-    :aws_access_key_id      => 'AKIAJRPCPX7VG2XJKS2A',
-    :aws_secret_access_key  => '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',
+    :aws_access_key_id      => 'K',
+    :aws_secret_access_key  => 'Tj',
      :region                 => 'us-west-2' # Change this for different AWS region. Default is 'us-east-1'
   }
-  config.fog_directory  = 'smiling-face'
+  config.fog_directory  = 'face'
 end
