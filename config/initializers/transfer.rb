54f7937127f09e7d440cdf6061cef4ab1685cb67
Lamine Kaba <Lkaba2003@yahoo.com>
2014-12-03 20:49:29 -0600

    changed some files

--- config/initializers/transfer.rb
+++ config/initializers/transfer.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => 'K',
-    :aws_secret_access_key  => 'Tj',
+    :provider               => 'lkaba',
+    :aws_access_key_id      => 'lkaba',
+    :aws_secret_access_key  => 'lkaba',
      :region                 => 'us-west-2' # Change this for different AWS region. Default is 'us-east-1'
   }
-  config.fog_directory  = 'face'
+  config.fog_directory  = 'lk'
 end
